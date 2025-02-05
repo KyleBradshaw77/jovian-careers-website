93f1a83944b379ac29bd3fc2655a0941ed4b3475
KyleBradshaw77 <160423935+KyleBradshaw77@users.noreply.github.com>
2025-02-05 02:34:48 +0000

    Functional and aesthetic improvements

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
+import duckdb
+import json
+import os
+import pandas as pd
 
 app = Flask(__name__)
+localdb = duckdb.connect(database=':memory:')
+
 
 JOBS = [
   {
@@ -29,15 +35,19 @@ JOBS = [
   }
 ]
 
+
+
 @app.route('/')
 def hello_world():
     return render_template('home.html',
                           jobs=JOBS,
                           company_name='Jovian')
 
+                           
 @app.route('/jobs')
 def list_jobs():
   return jsonify(JOBS)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
+  
